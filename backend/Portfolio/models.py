0ea21f0a785538a4392f3e341d4297bf09fc9e83
Patrick Odeh <vyltyan@gmail.com>
2025-07-25 06:42:09 +0100

    Add new tech icons for Python and React
    - Added a new JPEG image for Python in the tech_icons directory.
    - Added a new PNG image for React in the tech_icons directory.

--- backend/Portfolio/models.py
+++ backend/Portfolio/models.py
@@ -1,17 +1,23 @@
 from django.db import models
 
 
+class Tech(models.Model):
+    name = models.CharField(max_length=100)
+    icon = models.ImageField(upload_to='tech_icons/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+
 class Project(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     short_description = models.CharField(max_length=255, blank=True)
-    tech_stack = models.CharField(max_length=255, blank=True)
     github_link = models.URLField(blank=True)
     live_link = models.URLField(blank=True)
     image = models.ImageField(upload_to='projects/')
+    techs = models.ManyToManyField(Tech, related_name='projects')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
 
     def __str__(self):
         return self.name
